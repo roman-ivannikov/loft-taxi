feb317154bcb92d9128ea4772c20640a8c5403ae
Ivannikov Roman <roman.y.ivannikov@gmail.com>
2022-05-18 21:11:39 +0700

    Работа с наставником над ДЗ

--- src/App.js
+++ src/App.js
@@ -6,44 +6,37 @@ import RegistrationPage from './components/RegistrationPage';
 import MapPage from './components/MapPage';
 import ProfilePage from './components/ProfilePage';
 
+const listPages = {
+  map: { name: 'Карта', component: <MapPage/>, showInMenu: true },
+  profile: { name: 'Профиль', component: <ProfilePage/>, showInMenu: true },
+  login: { name: 'Войти', component: <LoginPage />, showInMenu: true },
+  registration: { name: 'Регистрация', component: <RegistrationPage />, showInMenu: false },
+}
+
+const menuItems = [];
+for (let key in listPages) {
+    if (listPages[key].showInMenu !== false) {
+      menuItems.push({ 'page': key, 'text': listPages[key].name } )
+    }
+};
 
 class App extends React.Component {
-  constructor( props ) {
-    super( props );
-    this.state = { currentPage: 'map' };
-
-    this.listPages = {
-      map: { name: 'Карта', component: <MapPage/>, showInMenu: true },
-      profile: { name: 'Профиль', component: <ProfilePage/>, showInMenu: true },
-      login: { name: 'Войти', component: <LoginPage goToPage={this.goToPage} />, showInMenu: true },
-      registration: { name: 'Регистрация', component: <RegistrationPage goToPage={this.goToPage} />, showInMenu: false },
-    };
-
-    const menuItems = [];
-    for (let key in this.listPages) {
-        if (this.listPages[key].showInMenu !== false) {
-          menuItems.push({ 'page': key, 'text': this.listPages[key].name } )
-        }
-    };
-    this.menuItems = menuItems;
-  }
-
-  Page = () => (
-    <section>
-      { this.listPages[ this.state.currentPage ].component }
-    </section>
-  );
+  state = { currentPage: 'map' };
 
   goToPage = ( page ) => {
     this.setState( { currentPage: page } );
   };
 
   render() {
+    const Page = () => listPages[ this.state.currentPage ].component;
+    
     return (
       <>
-        <Header pages={this.menuItems} currentPage={this.state.currentPage} goToPage={this.goToPage}/>
+        <Header pages={menuItems} currentPage={this.state.currentPage} goToPage={this.goToPage}/>
         <main>
-          <this.Page/>
+          <section>
+            <Page goToPage={this.goToPage} />
+          </section>
         </main>
       </>
     )
